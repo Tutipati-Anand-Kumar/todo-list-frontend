118978e7006b9fc4509625636da76314c1eff218
= <=>
2025-10-25 15:26:36 +0530

    frotend added

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -16,6 +16,7 @@ const Register = () => {
         password: "",
         repassword: ""
     });
+    
     const [show, setShow] = useState(true);
     const [passerror, setPassError] = useState([]);
     const [passMatchError, setPassMatchError] = useState(false);
